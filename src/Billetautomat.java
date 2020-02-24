684573c1ce1906d6daeaca1b420de26432c7027a
Woolbeach <christian.ulstrand@gmail.com>
2020-02-24 12:39:19 +0100

    Testing

--- src/Billetautomat.java
+++ src/Billetautomat.java
@@ -1,5 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Billetautomat {
     public static void main(String[] args) {
+
         System.out.println("Is this shit working?");
+        System.out.println("Maybe");
     }
+
 }

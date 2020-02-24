be0a66f06d8cc58c337497446a7dd960ac8a135f
Jonas Gehrke <jonasegehrke@gmail.com>
2020-02-24 14:04:13 +0100

    new commented header

--- src/Billetautomat.java
+++ src/Billetautomat.java
@@ -1,4 +1,9 @@
-
+/*--------------------------------------------------------------------------------
+|   Group:          Team extreme
+|   Names:          Christian Ulstrand (s195162), Filip Kristiansen (s195169) & Jonas Gehrke (s195155)
+|   Project:        Billetautomat
+|   Class purpose:  Main class
+ --------------------------------------------------------------------------------*/
 public class Billetautomat {
     public static void main(String[] args) {
 

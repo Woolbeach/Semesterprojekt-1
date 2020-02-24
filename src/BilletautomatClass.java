696fbe3cfb29a3728e3aceba9af4138df2c787a3
Fireray101 <filip261997@hotmail.com>
2020-02-24 14:42:41 +0100

    Filips maybe svar

--- src/BilletautomatClass.java
+++ src/BilletautomatClass.java
@@ -4,6 +4,21 @@ public class BilletautomatClass {
     private int balance;
     private int ticketsSold;
 
+    public int getPrice() {
+        return price;
+    } // Returns the price of a tickets current price
+
+    public void udskrivBillet() {                       // Prints out a ticket and the price of the ticket
+        System.out.println("##########B##T#########");  //
+        System.out.println("# BlueJ Trafikselskab #");
+        System.out.println("#                     #");
+        System.out.println("#        Billet       #");
+        System.out.println("#        " + pris + " kr.       #");
+        System.out.println("#                     #");
+        System.out.println("##########B##T#########");
+        System.out.println("##########B##T#########");
+        System.out.println();
+    }
     //SetPrice method
     public void setPrice(String accessCode, int newPrice) {
         if (accessCode.equals("1234")) price = newPrice;

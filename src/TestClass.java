da67d1872e4036eab642198b3d0b00ba734af56c
Woolbeach <christian.ulstrand@gmail.com>
2020-02-24 13:45:12 +0100

    Fra Chris

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
         System.out.println("hej");
         System.out.println("saodklja");
         System.out.println("Hej Jonas");
-        System.out.println(2askdlfjajk);
+        System.out.println("askdlfjajk");
         System.out.println("123123123123");
     }
 }

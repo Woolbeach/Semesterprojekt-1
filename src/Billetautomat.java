064a0f4cc1a4bda1cc655c5c88c5a9c97b7616bb
Jonas Gehrke <jonasegehrke@gmail.com>
2020-02-24 14:06:37 +0100

    delete testclass, fresh main

--- src/Billetautomat.java
+++ src/Billetautomat.java
@@ -7,12 +7,5 @@
 public class Billetautomat {
     public static void main(String[] args) {
 
-        System.out.println("Is this shit working?");
-        System.out.println("Maybe");
-        System.out.println("Filip: maybe yes?");
-        System.out.println("Hej med dig Filip");
-        System.out.println("Sidste test");
-        System.out.println("Jonas her");
-
     }
 }
--- src/TestClass.java
+++
@@ -1,9 +0,0 @@
-public class TestClass {
-    public static void main(String[] args) {
-        System.out.println("hej");
-        System.out.println("saodklja");
-        System.out.println("Hej Jonas");
-        System.out.println("askdlfjajk");
-        System.out.println("123123123123");
-    }
-}

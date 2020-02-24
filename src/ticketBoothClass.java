ea2591dd93c83285aedfa74ce5aa102bd436bb7f
Fireray101 <filip261997@hotmail.com>
2020-02-24 15:38:25 +0100

    Filips maybe svar

--- src/ticketBoothClass.java
+++ src/ticketBoothClass.java
@@ -3,7 +3,17 @@ public class ticketBoothClass {
     private double price;
     private double balance;
     private int ticketsSold;
-
+    //Constructor
+    java.util.Scanner scanObj = new java.util.Scanner(System.in);
+    public ticketBoothClass(double ticketPrice, double startBalance) {
+        price = ticketPrice;
+        balance = startBalance;
+        while (price < 0 || balance < 0){            //Checks for valid price before implementing
+            System.err.println("Invalid price and/or startBalance");
+            price=scanObj.nextDouble();
+            balance=scanObj.nextDouble();
+        }
+    }
     // Returns the price of a tickets current price
     public double getPrice() {
         return price;
@@ -21,17 +31,6 @@ public class ticketBoothClass {
         System.out.println("##########B##T#########");
         System.out.println();
     }
-    //Constructor
-    public ticketBoothClass(double ticketPrice, double startBalance) {
-        price = ticketPrice;
-        balance = startBalance;
-        while (ticketPrice < 0 || startBalance < 0){            //Checks for valid price before implementing
-            System.err.println("Invalid price and/or startBalance");
-
-        }
-
-    }
-
     //SetPrice method
     public void setPrice(String accessCode, double newPrice) {
         if (accessCode.equals("1234") && newPrice > 0) {            //if password is correct and price is > 0, the price is updated

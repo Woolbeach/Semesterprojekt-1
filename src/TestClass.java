a166b4c66795ef07546520de585ca61f8dbac1f5
Fireray101 <filip261997@hotmail.com>
2020-02-24 13:43:17 +0100

    Filips maybe svar

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("hej");
         System.out.println("saodklja");
+        for(int i=0;i<10;i++){
+
+        }
     }
 }

f28f202876bf1917dd164829c6a20ffdfcff9963
Woolbeach <christian.ulstrand@gmail.com>
2020-02-24 14:35:00 +0100

    Added billetAutomatClass.java

--- src/BilletautomatClass.java
+++ src/BilletautomatClass.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
 
 public class BilletautomatClass {
+    private int price;
+    private int balance;
+    private int ticketsSold;
+
+    //SetPrice method
+    public void setPrice(String accessCode, int newPrice) {
+        if (accessCode.equals("1234")) price = newPrice;
+        else System.err.println("Access denied - Wrong passcode");
+    }
 }

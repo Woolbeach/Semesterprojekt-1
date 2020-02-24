37f181b96ca289fe944b42583037baeedba62a95
Woolbeach <christian.ulstrand@gmail.com>
2020-02-24 14:30:26 +0100

    Added setPrice method

--- src/Billetautomat.java
+++ src/Billetautomat.java
@@ -5,7 +5,13 @@
 |   Class purpose:  Main class
  --------------------------------------------------------------------------------*/
 public class Billetautomat {
-    public static void main(String[] args) {
+    private int price;
+    private int balance;
+    private int ticketsSold;
 
+    //SetPrice method
+    public void setPrice(String accessCode, int newPrice) {
+        if (accessCode.equals("1234")) price = newPrice;
+        else System.err.println("Access denied - Wrong passcode");
     }
 }

f7023a37356ead925c28e268605ea6d8a5543d49
Fireray101 <filip261997@hotmail.com>
2020-02-24 13:17:03 +0100

    Filips maybe svar

--- src/Billetautomat.java
+++ src/Billetautomat.java
@@ -4,7 +4,8 @@ public class Billetautomat {
 
         System.out.println("Is this shit working?");
         System.out.println("Maybe");
-        System.out.println("Filip: maybe yes?")
+        System.out.println("Filip: maybe yes?");
+        System.out.println("Hej med dig Filip");
     }
 
 }
